2bf314fcf3803495c207906518456db33a7adc10
Alan Campora <acampora@medallia.com>
2020-03-14 02:25:59 -0300

    refactor(*): transformer done

--- index.js
+++ index.js
@@ -21,15 +21,19 @@ const got = require('got');
 const moment = require('moment');
 var { sleep } = require('sleep');
 const API = `https://covid19.mathdro.id/api/daily/`;
-const DATE_FORMAT = 'DD-MM-YYYY';
+const DATE_FORMAT = 'MM-DD-YYYY';
+
+const getNumber = stringNumber =>
+	stringNumber.length !== 0 ? parseInt(stringNumber) : 0;
 
 const proccess = async () => {
-	let current = moment('2020-01-02');
-	const today = moment();
+	let current = moment('01-22-2020');
+	let today = moment();
+	//const today = moment();
 	const result = {};
 
 	while (current <= today) {
-		await sleep(1);
+		//await sleep(1);
 		const currentDate = current.format(DATE_FORMAT);
 		const endpoint = `${API}${currentDate}`;
 		let data;
@@ -44,34 +48,48 @@ const proccess = async () => {
 			let resultCountryRegion = result[region.countryRegion];
 
 			if (resultCountryRegion) {
+				//check if date for country already exists
 				let currentDateDataForCountry =
 					result[region.countryRegion][currentDate];
 
 				if (currentDateDataForCountry) {
-					currentDateDataForCountry.confirmed += parseInt(region.confirmed);
-					currentDateDataForCountry.recovered += parseInt(region.recovered);
-					currentDateDataForCountry.deaths += parseInt(region.deaths);
+					// just sum
+					result[region.countryRegion][currentDate].confirmed += getNumber(
+						region.confirmed,
+					);
+					result[region.countryRegion][currentDate].recovered += getNumber(
+						region.recovered,
+					);
+					result[region.countryRegion][currentDate].deaths += getNumber(
+						region.deaths,
+					);
 				} else {
-					currentDateDataForCountry = {
-						confirmed: parseInt(region.confirmed),
-						recovered: parseInt(region.recovered),
-						deaths: parseInt(region.deaths),
-					};
+					// just create and initialize
+					result[region.countryRegion][currentDate] = {};
+					result[region.countryRegion][currentDate].confirmed = getNumber(
+						region.confirmed,
+					);
+					result[region.countryRegion][currentDate].recovered = getNumber(
+						region.recovered,
+					);
+					result[region.countryRegion][currentDate].deaths = getNumber(
+						region.deaths,
+					);
 				}
 			} else {
 				result[region.countryRegion] = {};
 				result[region.countryRegion][currentDate] = {
-					confirmed: parseInt(region.confirmed),
-					recovered: parseInt(region.recovered),
-					deaths: parseInt(region.deaths),
+					confirmed: getNumber(region.confirmed),
+					recovered: getNumber(region.recovered),
+					deaths: getNumber(region.deaths),
 				};
 			}
 		});
 
 		current.add(1, 'd');
-  }
+	}
 
-  console.log(result);
+	console.log(result);
 };
 
 proccess();
